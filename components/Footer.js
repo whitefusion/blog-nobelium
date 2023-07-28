59000276056bae53d8444e62bf2d0b7193eda629
Shin Bai <whitefusion@163.com>
2023-07-28 11:29:50 +0800

    feat: add 备案号 (#1)
    * feat: add 备案号
    
    * refactor: disable docker ci on push/pull

--- components/Footer.js
+++ components/Footer.js
@@ -1,15 +1,15 @@
-import { useConfig } from '@/lib/config'
-import Vercel from '@/components/Vercel'
+import { useConfig } from "@/lib/config";
+import Vercel from "@/components/Vercel";
 const Footer = ({ fullWidth }) => {
-  const BLOG = useConfig()
+  const BLOG = useConfig();
 
-  const d = new Date()
-  const y = d.getFullYear()
-  const from = +BLOG.since
+  const d = new Date();
+  const y = d.getFullYear();
+  const from = +BLOG.since;
   return (
     <div
       className={`mt-6 flex-shrink-0 m-auto w-full text-gray-500 dark:text-gray-400 transition-all ${
-        !fullWidth ? 'max-w-2xl px-4' : 'px-4 md:px-24'
+        !fullWidth ? "max-w-2xl px-4" : "px-4 md:px-24"
       }`}
     >
       <hr className="border-gray-200 dark:border-gray-600" />
@@ -18,11 +18,15 @@ const Footer = ({ fullWidth }) => {
           <p>
             © {BLOG.author} {from === y || !from ? y : `${from} - ${y}`}
           </p>
+          <a href="https://beian.miit.gov.cn/" target="_blank">
+            {" "}
+            沪ICP备20011311号-1{" "}
+          </a>
           <Vercel />
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
