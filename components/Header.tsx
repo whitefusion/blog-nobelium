69c400e0b1a10e48e43c037d4f1ed3fe0a7bfd71
xinbai <whitefusion@163.com>
2023-09-14 23:04:10 +0800

    fix: minor fix

--- components/Header.tsx
+++ components/Header.tsx
@@ -17,7 +17,7 @@ type Link = {
   name: string;
   to: string;
   show: boolean;
-  external?: string;
+  external?: boolean;
 };
 
 interface HeaderNameProps {
@@ -36,6 +36,7 @@ const NavBar = () => {
     { id: 0, name: locale.NAV.INDEX, to: BLOG.path || "/", show: true },
     { id: 1, name: locale.NAV.ABOUT, to: "/about", show: BLOG.showAbout },
     { id: 2, name: locale.NAV.SEARCH, to: "/search", show: true },
+    { id: 3, name: locale.NAV.RSS, to: "/feed", show: true, external: true },
   ];
   return (
     <div className="flex-shrink-0">
