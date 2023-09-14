df2c42ea33bb6f53cf2bbd4e6d2c36e86c10e324
xinbai <whitefusion@163.com>
2023-09-14 23:04:10 +0800

    fix: minor fix

--- blog.config.js
+++ blog.config.js
@@ -13,7 +13,7 @@ const BLOG = {
   darkBackground: "#18181B", // use hex value, don't forget '#'
   path: "", // leave this empty unless you want to deploy Nobelium in a folder
   since: 2021, // If leave this empty, current year will be used.
-  postsPerPage: 7,
+  postsPerPage: 9,
   sortByDate: true,
   showAbout: true,
   showArchive: true,
